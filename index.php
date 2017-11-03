3a4f64d5d6fdbd891ca790248512a439e1489285
talha945 <talhaqureshi71@gmail.com>
2017-11-03 19:09:10 +0500

    My third commmit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-     echo "php tests are here";
+     echo "php tests are here with";
         ?>
     </body>
 </html>
